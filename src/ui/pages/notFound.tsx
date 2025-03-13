0518ae13ad13e9fbfe95a770fc73639c744cffae
ITKmz <ittikorn.mutu@kmutt.ac.th>
2025-03-14 05:11:31 +0700

    feat: Add Index and Not Found pages with basic layout and navigation

---
+++ src/ui/pages/notFound.tsx
@@ -0,0 +1,21 @@
+import { Result, Button } from "antd";
+import { useNavigate } from "react-router-dom";
+import { FaFlag } from "react-icons/fa";
+
+export default function NotFoundPage() {
+  const navigate = useNavigate();
+
+  return (
+    <Result
+      icon={<FaFlag size={80} />}
+      title="404"
+      subTitle="Sorry, the page you visited does not exist."
+      extra={
+        <Button type="primary" onClick={() => navigate(-1)}>
+          Go Back
+        </Button>
+      }
+      className="align-center"
+    />
+  );
+}
