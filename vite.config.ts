95b2d2d0c64c89040306a9bd61494625f0e7812e
ITKmz <ittikorn.mutu@kmutt.ac.th>
2025-03-18 08:54:15 +0700

    chore(tsconfig): update configuration for composite builds and adjust paths

--- vite.config.ts
+++ vite.config.ts
@@ -2,11 +2,11 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react-swc';
 import tailwindcss from '@tailwindcss/vite';
 import path from 'path';
-import tsconfigPaths from 'vite-tsconfig-paths';
+// import tsconfigPaths from 'vite-tsconfig-paths';
 
 // https://vite.dev/config/
 export default defineConfig({
-    plugins: [tailwindcss(), react(), tsconfigPaths()],
+    plugins: [tailwindcss(), react()],
     base: './',
     build: {
         outDir: 'dist-react',
